5f00660d0b35f9c488b408951dbfe5aabe392160
fann133 <irfanfann005@gmail.com>
2025-05-15 16:00:52 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-4xl text-blue-500 text-center mt-10">Hello Tailwind di Laravel 10 🎉</h1>
+</body>
+</html>
